a33f3893ef2529e9d45cc9dd8242dd882200a2cf
Chandu <dulamchandu13@gmail.com>
2025-10-02 13:53:28 +0530

    fixed

--- RecipeTreasure/client/src/api.js
+++ RecipeTreasure/client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:7777",
+  baseURL: "https://recipetreasure.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
